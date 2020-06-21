f6cecca262f1398469bdf607dff7d02d1f25ce86
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-06-21 13:36:02 -0300

    Efetuando o one way databinding do input

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,15 +3,29 @@ import './Login.css';
 import twitterLogo from '../twitter.svg'
 
 export default class Login extends Component {
+  state = {
+    userName: '',
+
+  };
+
+  handleInputChange = (event) => {
+    this.setState({userName: event.target.value});
+  }
+
   render() {
     return (
       <div className="login-wrapper">
         <img src={twitterLogo} alt="GoTwitter" />
         
         <form>
-          <input placeholder="Nome de usuário" />
+          <input 
+            value={this.state.userName}
+            onChange={this.handleInputChange}
+            placeholder="Nome de usuário" />
 
           <button type="submit">Entrar</button>
+
+    <label>{this.state.userName}</label>
         </form>
       </div>
     );
