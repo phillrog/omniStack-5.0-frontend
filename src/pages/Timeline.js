38c557c429825328c4a9d939416b2186ef5b612e
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-06-21 13:44:54 -0300

    Montando layout da timeline

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,7 +1,34 @@
 import React, { Component } from 'react';
 
+import './Timeline.css';
+import twitterLogo from '../twitter.svg';
+
 export default class Timeline extends Component {
+  state = {
+    newTweet: '',
+  }
+
+  handleInputChange = (event) =>{
+    this.setState({newTweet: event.target.value});
+  }
+
+  handleNewTweet = (event) => {}
+
   render() {
-    return <h1>Timeline</h1>
+    return (
+      <div className="timeline-wrapper">
+        <img height={24} src={twitterLogo} alt="GoTwitter" />
+
+        <form>
+          <textarea
+            value={this.state.newTweet}
+            onChange={this.handleInputChange} 
+            onKeyDown={this.handleNewTweet}
+            placeholder="O que está acontecendo ?"
+           >
+          </textarea>
+        </form>
+      </div>
+    );
   }
 }
